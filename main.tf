942d732a388e47fe8ece7b4ba9f85b1c716458e5
Louie Lachica <llachica.578@gmail.com>
2023-01-27 15:29:14 -0500

    Add mysql DB entry to main.tf

--- main.tf
+++ main.tf
@@ -87,3 +87,32 @@ resource "azurerm_linux_virtual_machine" "nginx" {
    depends_on = [azurerm_resource_group.webserver]
    
 }
+
+resource "azurerm_mysql_server" "mysqlserver" {
+  name                = "mysqlserver"
+  location            = azurerm_resource_group.webserver.location
+  resource_group_name = azurerm_resource_group.webserver.name
+
+  administrator_login          = "mysqladmin"
+  administrator_login_password = "Admin123!"
+
+  sku_name   = "GP_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = true
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = false
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
+resource "azurerm_mysql_database" "mysqlDB" {
+  name                = "mysqlDB"
+  resource_group_name = azurerm_resource_group.webserver.name
+  server_name         = azurerm_mysql_server.mysqlserver.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
