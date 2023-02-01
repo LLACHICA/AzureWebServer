0919d1a64b1eeab0377818f6721f2145f24f1aa8
LLACHICA <llachica.578@gmail.com>
2023-02-01 11:21:35 -0500

    Add DNS Label

--- vnet.tf
+++ vnet.tf
@@ -19,6 +19,7 @@ resource "azurerm_public_ip" "webserver_public_ip" {
   location = var.location
   resource_group_name = azurerm_resource_group.webserver.name
   allocation_method   = "Dynamic"
+  domain_name_label = "hamza-gym"
 
   tags = {
     title = var.project
