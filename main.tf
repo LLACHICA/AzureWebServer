c2f05b10d5ef71a493262acc85776f2edde2879f
Louie Lachica <llachica.578@gmail.com>
2023-03-10 14:46:48 -0500

    fixing database code

--- main.tf
+++ main.tf
@@ -58,44 +58,47 @@ resource "azurerm_mysql_server" "mysqlDBserver" {
   storage_mb = 5120
   version    = "5.7"
 
-#  auto_grow_enabled                 = true
-#  backup_retention_days             = 7
-#  geo_redundant_backup_enabled      = true
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = true
   infrastructure_encryption_enabled = false
-  public_network_access_enabled     = false
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = false
+  minimal_tls_version               = "TLS1_2"
+  
 }
 
 resource "azurerm_mysql_database" "mysqlDB" {
   name                = "webserver-mysqldb"
   resource_group_name = azurerm_resource_group.webserver.name
   server_name         = azurerm_mysql_server.mysqlDBserver.name
-
-
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 
-  # Create a table to store the form data
-  provisioner "remote-exec" {
-        connection {
-      type        = "ssh"
-      host        = azurerm_mysql_server.mysqlDBserver.fqdn
-      user        = var.mysql_administrator_login
-      password    = var.mysql_administrator_login_password
-      agent       = false
-      timeout     = "30s"
-    }
-    inline = [
-      "mysql -u ${var.mysql_administrator_login} -p${var.mysql_administrator_login_password} -e \"CREATE TABLE gym_members (id INT(11) NOT NULL AUTO_INCREMENT, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL, start_date DATE NOT NULL, duration VARCHAR(50) NOT NULL, payment_type VARCHAR(50) NOT NULL, medical_notes TEXT, PRIMARY KEY (id));\" ${azurerm_mysql_database.mysqlDB.name}"
-    ]
-
-  }
 }
+
 resource "azurerm_mysql_firewall_rule" "mysqlFW" {
   name                = "AllowMyIP"
   resource_group_name = azurerm_resource_group.webserver.name
   server_name         = azurerm_mysql_server.mysqlDBserver.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
+}
+
+resource "azurerm_mysql_configuration" "members_table_config" {
+  name                = "members_table_config"
+  resource_group_name = azurerm_resource_group.webserver.name
+  server_name         = azurerm_mysql_server.mysqlDBserver.name
+  value               = "sql_mode=NO_ENGINE_SUBSTITUTION"
+}
+
+resource "null_resource" "create_members_table" {
+  depends_on = [
+    azurerm_mysql_database.mysqlDB,
+    azurerm_mysql_configuration.members_table_config
+  ]
+
+  provisioner "local-exec" {
+    command = "mysql -h ${azurerm_mysql_server.mysqlDBserver.fqdn} -u ${var.mysql_administrator_login} -p${var.mysql_administrator_login_password} -e 'CREATE TABLE gym_membership.members (id INT(11) NOT NULL AUTO_INCREMENT, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, email VARCHAR(100) NOT NULL, start_date DATE NOT NULL, duration VARCHAR(20) NOT NULL, payment_type VARCHAR(50) NOT NULL, medical_notes TEXT, PRIMARY KEY (id));'"
+  }
 }
