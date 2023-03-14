65212853c5c728d9220628094b34f5b8bc97c649
LLACHICA <llachica.578@gmail.com>
2023-03-14 15:02:07 -0400

    configure PHP

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,9 @@ output "public_ip_address" {
   value = azurerm_linux_virtual_machine.nginx.public_ip_address
 }
 
-output "mysql_database_hostname" {
-  value = azurerm_mariadb_server.mysqlDBserver.fqdn
-}
+#output "mysql_database_hostname" {
+#  value = azurerm_mariadb_server.mysqlDBserver.fqdn
+#}
 #output "tls_private_key" {
 # value     = tls_private_key.example_ssh.private_key_pem
 # sensitive = true
